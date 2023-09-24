7f4285c076aac7f182a51879fa777ff4ba714159
Roshan Senevirathne <roshaaanrulz@gmail.com>
2023-09-24 07:26:35 +0530

    task insertion successfully completed

--- app/src/main/java/com/example/taskmanager/fragments/AddTaskPopFragment.kt
+++ app/src/main/java/com/example/taskmanager/fragments/AddTaskPopFragment.kt
@@ -1,52 +1,95 @@
 package com.example.taskmanager.fragments
 
+import android.app.DatePickerDialog
 import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Spinner
+import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.fragment.app.DialogFragment
 import com.example.taskmanager.R
 import com.example.taskmanager.databinding.FragmentAddTaskPopBinding
-import com.google.firebase.database.DatabaseReference
+import com.google.android.material.textfield.TextInputEditText
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.StorageReference
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
 
-class AddTaskPopFragment : Fragment() {
+
+class AddTaskPopFragment : DialogFragment() {
     private lateinit var binding: FragmentAddTaskPopBinding
     private lateinit var storageRef: StorageReference
     private lateinit var firebaseFirestore: FirebaseFirestore
     private var imageUri: Uri?=null
+    private lateinit var listener: DialogSaveBtnClickListener
+
+    fun setListener(listener: homeFragment){
+        this.listener=listener
+    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         binding=FragmentAddTaskPopBinding.inflate(inflater,container,false)
-        return inflater.inflate(R.layout.fragment_add_task_pop, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        initVars()
         registerEvents()
     }
     private fun registerEvents(){
         var prioritySpinner: String? = null
         var categorySpinner: String? = null
-        uploadImage()
+        var description: String? = null
+        var tvDate: String? = null
+
         binding.saveBtn.setOnClickListener{
+            Log.d("debug 3","Save button clicked")
             val title=binding.titleEt.text.toString()
-            val description=binding.descriptionTxt.toString()
+            description=binding.descriptionTxt.text.toString()
             prioritySpinner = binding.prioritySpinner.selectedItem.toString()
             categorySpinner = binding.categorySpinner.selectedItem.toString()
+            tvDate=binding.tvDate.text.toString()
+            if(title.isNotEmpty()){
+                uploadImage()
+
+                listener.onSaveTask(title,binding.titleEt,
+                    description!!,binding.descriptionTxt,
+                    prioritySpinner!!,binding.prioritySpinner, categorySpinner!!,binding.categorySpinner,tvDate!!,binding.tvDate)
+            }else{
+                Toast.makeText(context,"Title cannot be empty",Toast.LENGTH_SHORT).show()
+            }
 
         }
         binding.selectImageBtn.setOnClickListener{
             resultLauncher.launch("image/*")
+        }
+
+        binding.tvDate.setOnClickListener {
+            val myCalendar=Calendar.getInstance()
+
+            val datePicker=DatePickerDialog.OnDateSetListener{view,year,month,dayOfMonth->
+                myCalendar.set(Calendar.YEAR,year)
+                myCalendar.set(Calendar.MONTH,month)
+                myCalendar.set(Calendar.DAY_OF_MONTH,dayOfMonth)
+                updateLable(myCalendar)
+            }
+            DatePickerDialog(requireContext(),datePicker,myCalendar.get(Calendar.YEAR),myCalendar.get(Calendar.MONTH),myCalendar.get(Calendar.DAY_OF_MONTH)).show()
+
+        }
+
+        binding.popUpClose.setOnClickListener{
+            dismiss()
         }
     }
     private fun initVars(){
@@ -60,6 +103,7 @@ class AddTaskPopFragment : Fragment() {
         binding.placeholdContainer.setImageURI(it)
     }
     private fun uploadImage(){
+        Log.d("debug 3","Upload Image called")
         storageRef=storageRef.child(System.currentTimeMillis().toString())
         imageUri?.let{
             storageRef.putFile(it).addOnCompleteListener{task->
@@ -69,7 +113,7 @@ class AddTaskPopFragment : Fragment() {
                         map["pic"]=uri.toString()
                         firebaseFirestore.collection("images").add(map).addOnCompleteListener {firestoreTask->
                             if(firestoreTask.isSuccessful){
-                                println(firestoreTask.result)
+                                Log.d("ImagePicker result",firestoreTask.result.toString())
                             }else{
 
                             }
@@ -86,4 +130,26 @@ class AddTaskPopFragment : Fragment() {
 
     }
 
+
+    private fun updateLable(myCalendar: Calendar){
+        Log.d("debug 3","Upload Label called")
+        val myFormat="dd-mm-yyyy"
+        val sdf=SimpleDateFormat(myFormat, Locale.ENGLISH)
+        binding.tvDate.setText(sdf.format(myCalendar.time))
+    }
+
+    interface DialogSaveBtnClickListener{
+        fun onSaveTask(
+            title:String,
+            titleEt:TextInputEditText,
+            description:String,
+            descriptionTxt:TextInputEditText,
+            priority:String,
+            prioritySpinner: Spinner,
+            category: String,
+            categorySpinner:Spinner,
+            date:String,
+            tvDate: TextView
+        )
+    }
 }
--- app/src/main/java/com/example/taskmanager/fragments/homeFragment.kt
+++ app/src/main/java/com/example/taskmanager/fragments/homeFragment.kt
@@ -1,25 +1,37 @@
 package com.example.taskmanager.fragments
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Spinner
+import android.widget.TextView
+import android.widget.Toast
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
-import com.example.taskmanager.R
 import com.example.taskmanager.databinding.FragmentHomeBinding
+import com.google.android.material.textfield.TextInputEditText
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 
 
-class homeFragment : Fragment() {
+class homeFragment : Fragment(), AddTaskPopFragment.DialogSaveBtnClickListener {
+    data class pushClass(
+        var title: String,
+        var description: String,
+        var priority: String,
+        var category: String,
+        var date:String,
+    )
 
     private lateinit var auth:FirebaseAuth
     private lateinit var  databaseRef:DatabaseReference
     private lateinit var navController: NavController
     private lateinit var binding:FragmentHomeBinding
+    private lateinit var popUpFragment: AddTaskPopFragment
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -38,11 +50,52 @@ class homeFragment : Fragment() {
         navController=Navigation.findNavController(view)
         auth=FirebaseAuth.getInstance()
         databaseRef=FirebaseDatabase.getInstance().reference
+            .child("Tasks").child(auth.currentUser?.uid.toString())
 
     }
     private fun registerEvents(){
+        Log.d("debug 3","debug line passed 0.5")
         binding.addBtn.setOnClickListener{
+            Log.d("debug 3","debug line passed 1")
+            popUpFragment=AddTaskPopFragment()
+            Log.d("debug 3","debug line passed 1.2")
+            popUpFragment.setListener(this)
+            Log.d("debug 3","debug line passed 2")
+            popUpFragment.show(
+                childFragmentManager,
+                "AddTaskPopFragment"
+            )
 
+        }
+    }
+
+    override fun onSaveTask(
+        title: String,
+        titleEt: TextInputEditText,
+        description: String,
+        descriptionTxt: TextInputEditText,
+        priority: String,
+        prioritySpinner: Spinner,
+        category: String,
+        categorySpinner: Spinner,
+        date: String,
+        tvDate: TextView
+    ) {
+        val pushObj=pushClass(
+            title=title,
+            description=description,
+            priority=priority,
+            category=category,
+            date=date
+            )
+        databaseRef.push().setValue(pushObj).addOnCompleteListener {
+            if(it.isSuccessful){
+                Toast.makeText(context,"Successfully saved",Toast.LENGTH_SHORT).show()
+                titleEt.text=null
+            }else{
+                Toast.makeText(context,it.exception?.message,Toast.LENGTH_SHORT).show()
+            }
+            popUpFragment.dismiss()
         }
     }
 
